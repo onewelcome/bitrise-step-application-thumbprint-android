e90114e80ec9508853dc3a03d676c8a646108633
Łukasz Łabuński <lukasz@onegini.com>
2020-07-08 16:08:50 +0200

    Initial implementation of calculating application thumbprint

--- step.sh
+++ step.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-set -ex
+set -e
 
-echo "This is 'onegini_artifactory_username': ${onegini_artifactory_username}"
-echo "This is 'onegini_artifactory_password': ${onegini_artifactory_password}"
-echo "This is 'calculator_version': ${calculator_version}"
-echo "This is 'application_path': ${application_path}"
-echo "This is 'temporary_path': ${temporary_path}"
+if [[ "${is_debug}" == "yes" ]] ; then
+  set -x
+fi
 
-result="0123456789ABCDEF"
+curlQuietParam="-sS"
+unzipQuietParam="-q"
+if [[ "${is_debug}" == "yes" ]] ; then
+  curlQuietParam=""
+  unzipQuietParam=""
+fi
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key ONEGINI_APP_THUMBPRINT --value $result
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+echo "Calculator version: ${calculator_version}"
+echo "Application path: ${application_path}"
+echo "Temporary path: ${temporary_path}"
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+calculator_url="https://repo.onegini.com/artifactory/onegini-sdk/com/onegini/mobile/sdk/android/android-app-signature-calculator/${calculator_version}/android-app-signature-calculator-${calculator_version}.jar"
+calculator_download_path="${temporary_path}/android-app-signature-calculator-${calculator_version}.jar"
+
+curl ${curlQuietParam} -u ${onegini_artifactory_username}:${onegini_artifactory_password} ${calculator_url}  -o ${calculator_download_path}
+application_thumbprint=$(java -jar ${calculator_download_path} ${application_path} -q)
+envman add --key ONEGINI_APP_THUMBPRINT --value "${application_thumbprint}"
