40d8d6bca65b8f526672cc3bce644ce29f6107a3
Łukasz Łabuński <lukasz@onegini.com>
2020-07-09 08:57:59 +0200

    Added killswitch functionality, removed unused property, fixed naming convention

--- step.sh
+++ step.sh
@@ -5,11 +5,14 @@ if [[ "${is_debug}" == "yes" ]] ; then
   set -x
 fi
 
-curlQuietParam="-sS"
-unzipQuietParam="-q"
+curl_quiet_param="-sS"
 if [[ "${is_debug}" == "yes" ]] ; then
-  curlQuietParam=""
-  unzipQuietParam=""
+  curl_quiet_param=""
+fi
+
+package_name_param=""
+if [ ! -z "${bundle_id}" ] ; then
+  package_name_param="-p"
 fi
 
 echo "Calculator version: ${calculator_version}"
@@ -19,6 +22,6 @@ echo "Temporary path: ${temporary_path}"
 calculator_url="https://repo.onegini.com/artifactory/onegini-sdk/com/onegini/mobile/sdk/android/android-app-signature-calculator/${calculator_version}/android-app-signature-calculator-${calculator_version}.jar"
 calculator_download_path="${temporary_path}/android-app-signature-calculator-${calculator_version}.jar"
 
-curl ${curlQuietParam} -u ${onegini_artifactory_username}:${onegini_artifactory_password} ${calculator_url}  -o ${calculator_download_path}
-application_thumbprint=$(java -jar ${calculator_download_path} ${application_path} -q)
+curl ${curl_quiet_param} -u ${onegini_artifactory_username}:${onegini_artifactory_password} ${calculator_url}  -o ${calculator_download_path}
+application_thumbprint=$(java -jar ${calculator_download_path} ${application_path} -q ${package_name_param} ${package_name})
 envman add --key ONEGINI_APP_THUMBPRINT --value "${application_thumbprint}"
